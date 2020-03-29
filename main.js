372dc1f2f5387bb72e16f6953d30635fb4efef1c
Jesse Pelzar <jessepelzar@Jesses-MBP-8662.home>
2020-03-29 17:29:38 -0400

    working with auto restart

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var myGamePiece;
 var myObstacles = [];
 var myScore;
-
+var roundsPlayed;
+// flappy
 function component(width, height, color, x, y, type) {
   this.width = width;
   this.height = height;
@@ -13,6 +14,7 @@ function component(width, height, color, x, y, type) {
   this.gravitySpeed = 0;
   this.type = type;
   this.score = 0;
+  this.round = 0;
   this.jumpMultiplier = 0;
   this.update = () => {
     ctx = myGameArea.context;
@@ -62,51 +64,73 @@ function component(width, height, color, x, y, type) {
 
 function startGame() {
   myGamePiece = new component(30, 30, "red", 100, 120);
-  myGamePiece.gravitySpeed = 4;
-  myGamePiece.jumpMultiplier = 3.7;
+  myGamePiece.gravitySpeed = 4.5;
+  myGamePiece.jumpMultiplier = 4.0;
   myGamePiece.gravity = 0;
   myScore = new component("30px", "Consolas", "black", 280, 40, "text");
+  roundsPlayed = new component("30px", "Consolas", "black", 280, 70, "text");
   myGameArea.start();
 }
 
 let myGameArea = {
   canvas: document.createElement("canvas"),
   start: function() {
+    // var background = new Image();
+
+    // Make sure the image is loaded first otherwise nothing will draw.
+    // background.onload = function(){
+    //     this.canvas.drawImage(background,0,0);
+    // }
+
     this.canvas.width = 480;
     this.canvas.height = 270;
     this.context = this.canvas.getContext("2d");
+    var background = new Image();
+    background.src = "images/bg.png";
+    this.context.drawImage(background,0,0);
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
   },
   clear: function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  reset: () => {
+    myGamePiece.y = 120;
+    myGamePiece.score = 0;
+    myObstacles = [];
+    myGamePiece.round++;
+    myGameArea.frameNo = 0;
   }
 }
 
 
 function updateGameArea() {
-  var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+  var x, height, gap, minHeight, maxHeight, minGap, maxGap, intervalVal, obsticalSpeedX;
   minHeight = 50;
-  maxHeight = 150;
-  gapHeight = 130;
+  maxHeight = 120;
+  gapHeight = 135;
+  intervalVal = 100;
+  obsticalSpeedX = -2.5;
 
-  for (i = 0; i < myObstacles.length; i += 1) {
+  for (let i = 0; i < myObstacles.length; i++) {
     if (myGamePiece.crashWith(myObstacles[i])) {
+      myGameArea.reset();
       return;
     }
   }
+
   myGameArea.clear();
   myGameArea.frameNo += 1;
-  if (myGameArea.frameNo == 1 || everyinterval(300)) {
+  if (myGameArea.frameNo == 1 || everyinterval(intervalVal)) {
     x = myGameArea.canvas.width;
     height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-    myObstacles.push(new component(50, height, "green", x, 0));
-    myObstacles.push(new component(50, x - height - gapHeight, "green", x, height + gapHeight));
+    myObstacles.push(new component(40, height, "green", x, 0));
+    myObstacles.push(new component(40, x - height - gapHeight, "green", x, height + gapHeight));
   }
   let t = true;
   for (let i = 0; i < myObstacles.length; i++) {
-    myObstacles[i].x += -1;
+    myObstacles[i].x += obsticalSpeedX;
     myObstacles[i].update();
     if (myObstacles[i].x + myObstacles[i].width == myGamePiece.x) {
       if (t === true) {
@@ -120,7 +144,9 @@ function updateGameArea() {
   setGamePieceSpeed();
 
   myScore.text = "SCORE: " + myGamePiece.score;
+  roundsPlayed.text = "ROUND: " + myGamePiece.round;
   myScore.update();
+  roundsPlayed.update();
   myGamePiece.newPos();
   myGamePiece.update();
 }
